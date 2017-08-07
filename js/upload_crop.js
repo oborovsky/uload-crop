0d8dd6de4fdba30e1a40f7cf7197caccb49b24c1
ulihtenshtein <ulihtenshtein@gmail.com>
2017-08-07 16:13:59 +0700

    добавил возможность редактировать уже загруженные картинки и загружать на место иконки другую картинку

--- js/upload_crop.js
+++ js/upload_crop.js
@@ -6,6 +6,7 @@ function Crop_Form(options)
 {
 	var elem = options.elem;
 	var open_btn = options.open_btn;
+	var isUpload = true;
 
 	var TARGET_W = options.target_w || 200;
 	var TARGET_H = options.target_h || 200;
@@ -23,11 +24,26 @@ function Crop_Form(options)
 	elem.on('click','#close_btn',onClose);
 	elem.on('input','#newName',onChangeName)
 
-	open_btn.on('click', onOpen);
+	open_btn.on('click', onOpen); 
+	//обработчик для таблицы уже загруженных картинок
+	$('#table').on('click', 'tr', onEditPhote);
+	elem.on('click','#overUpload', onOverUpload);
 
 	function onLoadFile(event)
 	{
-		loadFile(event.target.files);
+		if (isUpload)
+		{
+			loadFile(event.target.files);
+		}
+		else
+		{
+			loadOneFile(event.target.files);
+		}
+	}
+
+	function onOverUpload(event)
+	{
+		reloadFile();
 	}
 
 	function onSetSize(event)
@@ -50,6 +66,7 @@ function Crop_Form(options)
 
 	function onOpen()
 	{
+		isUpload = true;
 		open();
 	}
 
@@ -61,6 +78,9 @@ function Crop_Form(options)
 	//==========================================
 	function loadFile(files)
 	{
+		elem.find('#reload').val('false');
+		elem.find('#edit').val('false');
+		elem.find('#loadOneFile').val('false');
 		var file = files[0];
 		var name = file.name;
 
@@ -82,8 +102,9 @@ function Crop_Form(options)
 
 		elem.find('#name_img1').html(base_name[0] + '_thumb.' + base_name[1]);
 		elem.find('#name_img2').html(base_name[0] + '.' + base_name[1]);
-		elem.find('#img1').append($(img1));
-		elem.find('#img2').append($(img2));
+
+		elem.find('#img1').empty().append($(img1));
+	    elem.find('#img2').empty().append($(img2));			
 
 		var reader = new FileReader();
 
@@ -133,6 +154,75 @@ function Crop_Form(options)
 		}
 	}
 	//end loadFile
+	function loadOneFile(files)
+	{
+		elem.find('#loadOneFile').val('true');
+		elem.find('#edit').val('false');
+		elem.find('#reload').val('false');
+		var file = files[0];
+		var name = file.name;
+
+		var img1 = document.createElement('img');
+
+		img1.classList.add('obj');
+		img1.file = file;
+		img1.width = sizeImage;
+		
+		var base_name = elem.find('#name_img1').html().split('.');
+		base_name = base_name[0].split('_thumb');
+
+		elem.find('#newName').val(base_name[0]);	
+
+		elem.find('#img1').empty().append($(img1));
+
+		var reader = new FileReader();
+
+		reader.onload = onLoadReader([img1]);
+		reader.readAsDataURL(file);
+
+		function onLoadReader(imgs)
+		{
+			return function(e) {
+				imgs.forEach(function(img,i) {
+					img.src=e.target.result;
+				});
+
+				img = new Image();
+				img.src = e.target.result;
+				
+				img.onload = onLoadImage;
+
+				function onLoadImage()
+				{
+					source_w = this.width;
+					source_h = this.height;
+					elem.find('#source_h').val(source_h);
+					elem.find('#source_w').val(source_w);
+					elem.find('#sizeFile').val('исходный размер: ' + source_w + 'x' + source_h);
+					elem.find('#sizeSelectRect').html( TARGET_W + 'x' + TARGET_W);
+
+					try 
+					{
+						self.jcrop_api.destroy();
+					} 
+					catch (e) 
+					{
+							// object not defined
+					}
+					$('#img1 > img').Jcrop({
+				      trueSize:[source_w,source_h],
+				      aspectRatio:1,
+				      setSelect: [ 100, 100, TARGET_W, TARGET_H ],
+				      onSelect: updateCoords,
+				      onChange: updateCoords
+				    },function(){
+				        self.jcrop_api = this;
+				    });
+				};
+			};
+		}
+	}
+	//end loadOneFile
 	function setNewName(newName)
 	{
 		elem.find('#name_img1').html(newName + '_thumb.jpg');
@@ -169,6 +259,7 @@ function Crop_Form(options)
 	function close()
 	{
 		elem.hide();	
+		elem.find('#overUpload').hide();
 	}
 
 	function open()
@@ -176,32 +267,131 @@ function Crop_Form(options)
 		elem.find('#reset_btn').click();
 		elem.show();	
 	}
+	//обработка уже загруженных на сервер файлов
+	function onEditPhote(event)
+	{
+		var tr = event.target;
+		var i = 20;
+		while( tr.tagName.toUpperCase() != "TR" && i > 0) {
+			tr = tr.parentElement;
+			i--;
+		}
+		if ( i == 0) {
+			console.log('не нашел строку в таблице, содеращей картинки');
+			return true;
+		}
+		var img1 = tr.querySelectorAll('img')[0];
+		var img2 = tr.querySelectorAll('img')[1];
+		console.log('img1:', img1.src);
+		console.log('img2:',img2.src);
+		editPhoteInTable(img1.src, img2.src)
+
+
+	}
+	function editPhoteInTable(src1, src2)
+	{
+		isUpload = false;
+
+		elem.find('#reload').val('false');
+		elem.find('#loadOneFile').val('false');
+		elem.find('#edit').val('true');
+
+		elem.find('#overUpload').show();
+
+		open();
+
+		var img1 = document.createElement('img');
+		var img2 = document.createElement('img');
+
+		img1.classList.add('saved');
+		img1.src = src1;
+		img2.classList.add('saved');
+		img2.src = src2;
+		img1.width = img2.width = sizeImage;
+		
+		var base_name1 = src1.split('/');
+		var base_name2 = src2.split('/');
+		var base_name = (base_name2[base_name2.length-1]).split('.');
+		var newName = elem.find('#newName').val();
+		if (newName.length > 0 ) 
+		{
+			base_name[0] = newName;
+		}
+
+		elem.find('#name_img1').html(base_name[0] + '_thumb.' + base_name[1]);
+		elem.find('#name_img2').html(base_name[0] + '.' + base_name[1]);
+		elem.find('#img1').append($(img1));
+		elem.find('#img2').append($(img2));
+		elem.find('#srcName').val(src1);
+		var img = new Image();
+		img.src = src1;
+		source_w = img.width;
+		source_h = img.height;
+		img.src = src2;
+		elem.find('#source_h').val(source_h);
+		elem.find('#source_w').val(source_w);
+		elem.find('#sizeFile').val('исходный размер: ' + img.width + 'x' + img.height);
+		elem.find('#sizeSelectRect').html( TARGET_W + 'x' + TARGET_W);
+
+		try 
+		{
+			self.jcrop_api.destroy();
+		} 
+		catch (e) 
+		{
+				// object not defined
+		}
+		$('#img1 > img').Jcrop({
+	      trueSize:[source_w,source_h],
+	      aspectRatio:1,
+	      setSelect: [ 100, 100, TARGET_W, TARGET_H ],
+	      onSelect: updateCoords,
+	      onChange: updateCoords
+	    },function(){
+	        self.jcrop_api = this;
+	    });
+		
+	}
+
+	function reloadFile()
+	{
+		elem.find('#reload').val('true');
+		elem.find('#edit').val('false');
+		elem.find('#loadOneFile').val('false');
+
+		var img1 = elem.find('#img1 > img').get(0);
+		var img2 = elem.find('#img2 > img').get(0);
+		img1.src = img2.src;
+
+		var img = new Image();
+		img.src = img2.src;
+		source_w = img.width;
+		source_h = img.height;
+		elem.find('#source_h').val(source_h);
+		elem.find('#source_w').val(source_w);
+		elem.find('#srcName').val(img2.src);
+		elem.find('#sizeSelectRect').html( TARGET_W + 'x' + TARGET_W);
+
+		try 
+		{
+			self.jcrop_api.destroy();
+		} 
+		catch (e) 
+		{
+				// object not defined
+		}
+		$('#img1 > img').Jcrop({
+	      trueSize:[source_w,source_h],
+	      aspectRatio:1,
+	      setSelect: [ 100, 100, TARGET_W, TARGET_H ],
+	      onSelect: updateCoords,
+	      onChange: updateCoords
+	    },function(){
+	        self.jcrop_api = this;
+	    });
+	}
 }
 //end class Crop_Form
 
 var crop_form = new Crop_Form({ elem:$('#popup_upload'),open_btn:$('#open_btn')});
-$('#table').on('click', 'tr', onEditPhote);
-function onEditPhote(event)
-{
-	var tr = event.target;
-	var i = 20;
-	while( tr.tagName.toUpperCase() != "TR" && i > 0) {
-		tr = tr.parentElement;
-		i--;
-	}
-	if ( i == 0) {
-		console.log('не нашел строку в таблице, содеращей картинки');
-		return true;
-	}
-	var img1 = tr.querySelectorAll('img')[0];
-	var img2 = tr.querySelectorAll('img')[1];
-	console.log('img1:', img1.src);
-	console.log('img2:',img2.src);
-	editPhoteInTable(img1.src, img2,src)
-
-
-}
-function editPhoteInTable(src1, src2)
-{
 	
-}
--- upload_file.php
+++ upload_file.php
@@ -7,17 +7,16 @@ echo "start";
 $dir = 'images/';
 $src = $_FILES['phote']['tmp_name'];
 $name =$_FILES['phote']['name'];
-if ( isset($_POST['newName']) )
+$name = explode(".",$name);
+$name = $name[0];
+if ( $_POST['newName'] <> '' )
 {
 	$name = $_POST['newName'];
 }
 
-$photo_dest = 'images/'.$name.'.jpg';
-	// copy the photo from the tmp path to our path
-copy($src, $photo_dest);
-
-echo "part1";
-
+$reload = $_POST['reload'];
+$edit = $_POST['edit'];
+$loadOneFile = $_POST['loadOneFile'];
 
 $targ_w = $_POST['target_w'];
 $targ_h = $_POST['target_h'];
@@ -30,18 +29,148 @@ $aspectRation  = $source_w / $source_h;
 $jpeg_quality = 90;
 // photo path
 echo "part2";
-// create new jpeg image based on the target sizes
-$img_r = imagecreatefromjpeg($src);
-$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
-echo "part3";
-// crop photo
-imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'], $targ_w,$targ_h,$_POST['w'],$_POST['h']*$aspectRation);
-// create the physical photo
-imagejpeg($dst_r,$src,$jpeg_quality);
-echo "part4";
-$photo_dest = 'images/'.$name.'_thumb.jpg';
-copy($src, $photo_dest);
-echo "Ok";
+
+if ( $reload == 'false' && $edit == 'false' && $loadOneFile == 'false' )
+{	echo 'upload<br>';
+	uploadFile();
+} elseif ($reload == 'true') {
+	echo 'reload<br>';
+	reloadFile();
+} elseif ($edit == 'true') {
+	echo 'edit<br>';
+	editFile();
+} elseif ($loadOneFile == 'true') {
+	loadOneFile();
+}
+
+function uploadFile()
+{
+	global $src, $targ_w, $targ_h, $aspectRation, $jpeg_quality, $name;
+
+	$photo_dest = 'images/'.$name.'.jpg';
+		// copy the photo from the tmp path to our path
+	copy($src, $photo_dest);
+
+	// create new jpeg image based on the target sizes
+	$img_r = imagecreatefromjpeg($src);
+	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
+	echo "part3";
+	// crop photo
+	imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'], $targ_w,$targ_h,$_POST['w'],$_POST['h']*$aspectRation);
+	// create the physical photo
+	imagejpeg($dst_r,$src,$jpeg_quality);
+	echo "part4";
+	$photo_dest = 'images/'.$name.'_thumb.jpg';
+	echo $photo_dest;
+	copy($src, $photo_dest);
+	echo "Ok";
+}
+function reloadFile()
+{
+	global $targ_w, $targ_h, $aspectRation, $jpeg_quality;
+	$srcEdit = $_POST['srcName'];
+
+	echo $srcEdit.'<br>';
+	$name = explode("/",$srcEdit);
+	print_r($name);
+	$src = 'images/'.$name[count($name) -1 ];
+	echo $src;
+	$img_r = imagecreatefromjpeg($src);
+	if( !$img_r )
+	{
+		echo ' error on imagecreatefromjpeg ';
+	}
+
+	echo " width:$targ_w, height:$targ_h ";
+
+	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
+	if( !$dst_r )
+	{
+		echo ' error on ImageCreateTrueColor ';
+	}
+	echo "part3";
+	// crop photo
+	$succ = imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'], $targ_w,$targ_h,$_POST['w'],$_POST['h']*$aspectRation);
+	if ( !$succ)
+	{
+		echo ' error on imagecopyresampled ';
+	}
+	// create the physical photo
+	$src = explode(".",$src);
+	$src_r = $src[0]."_thumb.jpg";
+	$succ = imagejpeg($dst_r,$src_r,$jpeg_quality);
+	if ( !$succ)
+	{
+		echo ' error on imagejpeg ';
+	}
+	echo "part4";
+	// $photo_dest = 'images/'.$name.'_thumb.jpg';
+	// copy($src, $photo_dest);
+	echo "Ok";	
+
+}
+
+function editFile()
+{
+	global $targ_w, $targ_h, $aspectRation, $jpeg_quality;
+	$srcEdit = $_POST['srcName'];
+
+	echo $srcEdit.'<br>';
+	$name = explode("/",$srcEdit);
+
+	$src = 'images/'.$name[count($name) -1 ];
+	echo $src;
+	$img_r = imagecreatefromjpeg($src);
+	if( !$img_r )
+	{
+		echo ' error on imagecreatefromjpeg ';
+	}
+
+	echo " width:$targ_w, height:$targ_h ";
+
+	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
+	if( !$dst_r )
+	{
+		echo ' error on ImageCreateTrueColor ';
+	}
+	echo "part3";
+	// crop photo
+	$succ = imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'], $targ_w,$targ_h,$_POST['w'],$_POST['h']*$aspectRation);
+	if ( !$succ)
+	{
+		echo ' error on imagecopyresampled ';
+	}
+	// create the physical photo
+	$succ = imagejpeg($dst_r,$src,$jpeg_quality);
+	if ( !$succ)
+	{
+		echo ' error on imagejpeg ';
+	}
+	echo "part4";
+	// $photo_dest = 'images/'.$name.'_thumb.jpg';
+	// copy($src, $photo_dest);
+	echo "Ok";
+}
+
+function loadOneFile()
+{
+	global $src, $targ_w, $targ_h, $aspectRation, $jpeg_quality, $name;
+
+
+	// create new jpeg image based on the target sizes
+	$img_r = imagecreatefromjpeg($src);
+	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
+	echo "part3";
+	// crop photo
+	imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'], $targ_w,$targ_h,$_POST['w'],$_POST['h']*$aspectRation);
+	// create the physical photo
+	imagejpeg($dst_r,$src,$jpeg_quality);
+	echo "part4";
+	$photo_dest = 'images/'.$name.'_thumb.jpg';
+	echo $photo_dest;
+	copy($src, $photo_dest);
+	echo "Ok";
+}
 ?>
 </body>
 </html>
